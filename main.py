abbf2bd876f50ad346ac816c0f9d7ce4c9c3c078
Raphael Raja <jrrphonix@gmail.com>
2022-05-21 00:48:49 +0530

    Random Quote generation for monday along with mail connectivity is completed

---
+++ main.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = ""
+PASSWORD = ""
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs="",
+            msg=f"Subject:Monday Motivation\n\n{quote}")
+
